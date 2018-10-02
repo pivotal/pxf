2c29d236c39d64f1ba25301669ba0ab37e3de7ac
Francisco Guerrero <aguerrero@pivotal.io>
2018-10-02 09:17:35 -0700

    Read hadoop namenode variable from env

--- concourse/scripts/load_data.bash
+++ concourse/scripts/load_data.bash
@@ -1,18 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cat << EOF > /tmp/gsc-ci-service-account.key.json
 ${GOOGLE_JSON_KEY}
 EOF
 
 ~/gcloud/google-cloud-sdk/bin/gcloud \
-    auth activate-service-account \
-    --key-file=/tmp/gsc-ci-service-account.key.json
+  auth activate-service-account \
+  --key-file=/tmp/gsc-ci-service-account.key.json
 
 set -x
 
-hadoop distcp gs://data-gpdb-ud-tpch/${SCALE}/lineitem_data/*.tbl /tmp/lineitem_read/
+HADOOP_HOSTNAME="ccp-$(cat terraform_dataproc/name)-m"
 
-
-~/gcloud/google-cloud-sdk/bin/gsutil \
-    -o GSUtil:parallel_composite_upload_threshold=150M \
-    cp ${data}/${scale}/lineitem${i}.tbl gs://data-gpdb-ud-tpch/${scale}/lineitem_data/ &
+~/gcloud/google-cloud-sdk/bin/gcloud compute \
+  --project "data-gpdb-ud" \
+  ssh \
+  --zone "us-central1-a" ${HADOOP_HOSTNAME} \
+  --command "hadoop distcp gs://data-gpdb-ud-tpch/${SCALE}/lineitem_data/*.tbl /tmp/lineitem_read/"
