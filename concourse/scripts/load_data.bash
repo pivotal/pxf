f5be3ef1a577a89b509a38e33090906501c16e4a
Francisco Guerrero <aguerrero@pivotal.io>
2018-10-02 10:35:05 -0700

    Update gcloud command

--- concourse/scripts/load_data.bash
+++ concourse/scripts/load_data.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 cat << EOF > /tmp/gsc-ci-service-account.key.json
-${GOOGLE_JSON_KEY}
+${GOOGLE_CREDENTIALS}
 EOF
 
-~/gcloud/google-cloud-sdk/bin/gcloud \
-  auth activate-service-account \
+gcloud auth activate-service-account \
   --key-file=/tmp/gsc-ci-service-account.key.json
 
 set -x
 
 HADOOP_HOSTNAME="ccp-$(cat terraform_dataproc/name)-m"
 
-~/gcloud/google-cloud-sdk/bin/gcloud compute \
-  --project "data-gpdb-ud" \
-  ssh \
-  --zone "us-central1-a" ${HADOOP_HOSTNAME} \
+echo -e "Y\n\n\n" | gcloud compute --project "data-gpdb-ud" ssh \
+  --force-key-file-overwrite --zone "us-central1-a" ccp-ci-service@${HADOOP_HOSTNAME} \
   --command "hadoop distcp gs://data-gpdb-ud-tpch/${SCALE}/lineitem_data/*.tbl /tmp/lineitem_read/"
+
+ssh -t -i ~/.ssh/google_compute_engine ccp-ci-service@${HADOOP_HOSTNAME} \
+  "hadoop distcp gs://data-gpdb-ud-tpch/${SCALE}/lineitem_data/*.tbl /tmp/lineitem_read/"
