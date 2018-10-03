c8432842314f44b765da75a46f1c3bf46771fc5f
Francisco Guerrero <aguerrero@pivotal.io>
2018-10-03 08:31:33 -0700

    Fix ssh key setting issue

--- concourse/scripts/load_data.bash
+++ concourse/scripts/load_data.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+yum install -y -d1 openssh openssh-clients
+mkdir -p ~/.ssh/
+ssh-keygen -b 4096 -t rsa -f ~/.ssh/google_compute_engine -N "" -C "ccp-ci-service"
+
 cat << EOF > /tmp/gsc-ci-service-account.key.json
 ${GOOGLE_CREDENTIALS}
 EOF
@@ -11,25 +15,10 @@ set -x
 
 HADOOP_HOSTNAME="ccp-$(cat terraform_dataproc/name)-m"
 
-yum install -y -d1 openssh openssh-clients
-mkdir -p ~/.ssh/
-ssh-keygen -b 4096 -t rsa -f ~/.ssh/google_compute_engine -N "" -C "ccp-ci-service"
-
 gcloud compute instances add-metadata ${HADOOP_HOSTNAME} \
-  --metadata ssh-keys="$(cat ~/.ssh/google_compute_engine.pub)" \
+  --metadata ssh-keys="ccp-ci-service:$(cat ~/.ssh/google_compute_engine.pub)" \
   --zone "us-central1-a"
-
-
-
-#echo -e "Y\n\n\n" | gcloud compute --project "data-gpdb-ud" ssh \
-#  --force-key-file-overwrite --zone "us-central1-a" ccp-ci-service@${HADOOP_HOSTNAME} \
-#  --command "hadoop distcp gs://data-gpdb-ud-tpch/${SCALE}/lineitem_data/*.tbl /tmp/lineitem_read/"
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   -t -i ~/.ssh/google_compute_engine ccp-ci-service@${HADOOP_HOSTNAME} \
   "hadoop distcp gs://data-gpdb-ud-tpch/${SCALE}/lineitem_data/*.tbl /tmp/lineitem_read/" || exit 1
-
-
-#gcloud dataproc jobs submit hadoop --cluster ${HADOOP_HOSTNAME} \
-#  --region "us-central1" --class distcp \
-#  -- gs://data-gpdb-ud-tpch/${SCALE}/lineitem_data/*.tbl /tmp/lineitem_read/
